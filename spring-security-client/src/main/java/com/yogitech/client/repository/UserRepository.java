c21f4b5907bfbcc4bba70425db9d23988ce51e24
Yogi8887 <yogi.nit.mca@gmail.com>
2023-01-26 12:53:29 +0530

    feat-1: spring security client server

---
+++ spring-security-client/src/main/java/com/yogitech/client/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.yogitech.client.repository;
+
+import com.yogitech.client.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
